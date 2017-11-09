256cb69df8da679592e7db43390a799a7d13a14f
Cheryln <cherylnbarber@gmail.com>
2017-11-09 11:28:59 -0800

    Changed reference to controller on announcement to ctrl.

--- routes/announcement.js
+++ routes/announcement.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router();
-const announcement = require('../controllers/ctrl_announcement.js')
+const ctrl = require('../controllers/ctrl_announcement.js')
 
 /* GET home page. */
-router.get('/', announcement.getAllAnnouncements);
+router.get('/', ctrl.getAllAnnouncements);
 
 module.exports = router;
