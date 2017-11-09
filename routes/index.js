0b0d6edcb6e2339ae6211ceb77f07fc07142a644
Cheryln <cherylnbarber@gmail.com>
2017-11-09 11:48:00 -0800

    Gave a message on the localhost:3000 route.

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Express' });
-// });
+router.get('/', function(req, res, next) {
+  res.send('Welcome to the BOS site.  This is the index page and we have no routes written for this page as of yet.');
+});
 
 module.exports = router;
