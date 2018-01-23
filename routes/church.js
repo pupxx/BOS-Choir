5db8561ff13c0648fdcb591e054d2eb9beb02be8
Cheryln <cherylnbarber@gmail.com>
2018-01-23 13:51:06 -0800

    Wired up church.js to handle requiring Authentication for routes.

--- routes/church.js
+++ routes/church.js
@@ -1,9 +1,14 @@
-var express = require('express');
-var router = express.Router();
-const ctrl = require('../controllers/ctrl_church.js')
+const express = require('express');
+
+const router = express.Router();
+const ctrl = require('../controllers/ctrl_church.js');
+const passport = require('passport');
+const passportService = require('../services/passport');
+
+const requireAuth = passport.authenticate('jwt', { session: false });
 
 /* GET home page. */
-router.get('/', ctrl.getAllChurches);
-router.get('/:id', ctrl.getOneChurch);
+router.get('/', requireAuth, ctrl.getAllChurches);
+router.get('/:id', requireAuth, ctrl.getOneChurch);
 
 module.exports = router;
